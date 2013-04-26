abaa40449079d7cab31ffe7871a35bfdba987a0d
Oskar Kristensson <oskarsvantek@gmail.com>
2013-04-26 12:23:22 +0200

    Launcher D2 P1
    Försöker lägga in lite grafik i launchern o se om man kan spara den
    inuti jar-filen.

--- Eclipse Workspace/Launcher/src/Screen.java
+++ Eclipse Workspace/Launcher/src/Screen.java
@@ -2,20 +2,36 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JPanel;
 
 
 public class Screen extends JPanel{
+	
+	public Screen(){
+		loadResources();
+	}
 
 	public void paint(Graphics g){
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setPaint(Color.BLACK);
 		g2d.fill(new Rectangle2D.Float(0, 0, getWidth(), getHeight()));
-		
+		g2d.drawImage(background, 0, 0, getWidth(), getHeight(), null);
 		g2d.setPaint(Color.GRAY);
 		g2d.fill(new Rectangle2D.Float(10, 10, getWidth() - 20, 300));
+		g2d.drawImage(fill, 10, 10, getWidth() - 20, 300, null);
 		g2d.fill(new Rectangle2D.Float(10, getHeight() - 115, getWidth() - 20, 100));
+		g2d.drawImage(fill, 10, getHeight() - 115, getWidth() - 20, 100, null);
+		g2d.setPaint(Color.LIGHT_GRAY);
+		g2d.drawRect(10, 10, getWidth() - 20, 300);
+		g2d.drawRect(10, getHeight() - 115, getWidth() - 20, 100);
+		g2d.setPaint(Color.DARK_GRAY);
+		g2d.drawString("Settings", getWidth() / 2 - 25, 20);
+		g2d.drawString("Controls", getWidth() / 2 - 25, getHeight() - 20);
 		
 		for(int i = 0; i < Main.buttons.size(); i++){
 			Button B = Main.buttons.get(i);
@@ -34,8 +50,17 @@ public class Screen extends JPanel{
 			}
 			g2d.drawString(B.TITLE, B.BOX.x + 2, B.BOX.y + 14);
 		}
-		
-		g2d.setColor(Color.RED);
-		g2d.drawRect(Main.mouse.x, Main.mouse.y, 1, 1);
+	}
+	
+	private BufferedImage background;
+	private BufferedImage fill;
+	
+	public void loadResources(){
+		try{
+			background = ImageIO.read(new File("background.png"));
+			fill = ImageIO.read(new File("fill.png"));
+		}catch(Exception ex){
+			ex.printStackTrace(System.out);
+		}
 	}
 }
